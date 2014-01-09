ac898fe232776520fd67076538521cd23c43d6ca
Chris Montgomery <christopher.montgomery@dowjones.com>
2014-01-09 13:18:14 -0600

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('djxDashApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
